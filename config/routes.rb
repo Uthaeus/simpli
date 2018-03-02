89fb8097ddeb46a76bb07ca4dcbca2dfdcd0ab8d
Uthaeus <romanlavery@gmail.com>
2018-03-01 18:46:20 -0700

    Implemented nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   get 'pages/form'
 
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  
   resources :requirements
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
