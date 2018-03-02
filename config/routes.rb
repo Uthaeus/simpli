26890a6f4a3001699ca30a0e0f0d2a13c6eabeef
Uthaeus <romanlavery@gmail.com>
2018-03-01 18:16:44 -0700

    generated pages controller, reset root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/form'
+
   devise_for :users
   resources :requirements
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'requirements#index'
+  root to: 'pages#home'
 end
